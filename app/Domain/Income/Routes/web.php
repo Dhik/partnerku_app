d56fa99eb933c4a3cfb45e63cb0dcf9a3c240aee
Dhik <mohammad18018@mail.unpad.ac.id>
2025-06-09 10:09:48 +0700

    update

--- app/Domain/Income/Routes/web.php
+++ app/Domain/Income/Routes/web.php
@@ -8,16 +8,17 @@ use App\Domain\Income\Controllers\IncomeController;
 | Income Routes
 |--------------------------------------------------------------------------
 */
-
-Route::prefix('income')
+Route::prefix('admin')
     ->middleware('auth')
-    ->group(function () {
-        Route::get('/', [IncomeController::class, 'index'])->name('income.index');
-        Route::get('/data', [IncomeController::class, 'data'])->name('income.data');
-        Route::get('/create', [IncomeController::class, 'create'])->name('income.create');
-        Route::post('/', [IncomeController::class, 'store'])->name('income.store');
-        Route::get('/{income}', [IncomeController::class, 'show'])->name('income.show');
-        Route::get('/{income}/edit', [IncomeController::class, 'edit'])->name('income.edit');
-        Route::put('/{income}', [IncomeController::class, 'update'])->name('income.update');
-        Route::delete('/{income}', [IncomeController::class, 'destroy'])->name('income.destroy');
-    });
+        ->group(function () {
+            Route::prefix('income')->group(function () {
+                Route::get('/', [IncomeController::class, 'index'])->name('income.index');
+                Route::get('/data', [IncomeController::class, 'data'])->name('income.data');
+                Route::get('/create', [IncomeController::class, 'create'])->name('income.create');
+                Route::post('/', [IncomeController::class, 'store'])->name('income.store');
+                Route::get('/{income}', [IncomeController::class, 'show'])->name('income.show');
+                Route::get('/{income}/edit', [IncomeController::class, 'edit'])->name('income.edit');
+                Route::put('/{income}', [IncomeController::class, 'update'])->name('income.update');
+                Route::delete('/{income}', [IncomeController::class, 'destroy'])->name('income.destroy');
+            });
+});
--- app/Domain/Payroll/Routes/web.php
+++ app/Domain/Payroll/Routes/web.php
@@ -9,15 +9,17 @@ use App\Domain\Payroll\Controllers\PayrollController;
 |--------------------------------------------------------------------------
 */
 
-Route::prefix('payroll')
+Route::prefix('admin')
     ->middleware('auth')
-    ->group(function () {
-        Route::get('/', [PayrollController::class, 'index'])->name('payroll.index');
-        Route::get('/data', [PayrollController::class, 'data'])->name('payroll.data');
-        Route::get('/create', [PayrollController::class, 'create'])->name('payroll.create');
-        Route::post('/', [PayrollController::class, 'store'])->name('payroll.store');
-        Route::get('/{payroll}', [PayrollController::class, 'show'])->name('payroll.show');
-        Route::get('/{payroll}/edit', [PayrollController::class, 'edit'])->name('payroll.edit');
-        Route::put('/{payroll}', [PayrollController::class, 'update'])->name('payroll.update');
-        Route::delete('/{payroll}', [PayrollController::class, 'destroy'])->name('payroll.destroy');
-    });
+        ->group(function () {
+            Route::prefix('payroll')->group(function () {
+                Route::get('/', [PayrollController::class, 'index'])->name('payroll.index');
+                Route::get('/data', [PayrollController::class, 'data'])->name('payroll.data');
+                Route::get('/create', [PayrollController::class, 'create'])->name('payroll.create');
+                Route::post('/', [PayrollController::class, 'store'])->name('payroll.store');
+                Route::get('/{payroll}', [PayrollController::class, 'show'])->name('payroll.show');
+                Route::get('/{payroll}/edit', [PayrollController::class, 'edit'])->name('payroll.edit');
+                Route::put('/{payroll}', [PayrollController::class, 'update'])->name('payroll.update');
+                Route::delete('/{payroll}', [PayrollController::class, 'destroy'])->name('payroll.destroy');
+            });
+});
